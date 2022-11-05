165731ec11a7f1973e3f97fe019a6c858923ba62
aboude <53794997+aboudekahil@users.noreply.github.com>
2022-11-05 08:14:06 +0200

    basics

---
+++ configuration/config.js
@@ -0,0 +1,12 @@
+const config = {
+  PORT: 3306,
+  HOST: 'localhost',
+  USER: 'root',
+  PASSWORD: 'root',
+  DB: 'csis_228_lb',
+  multipleStatements: true,
+};
+
+module.exports = {
+  config: config,
+};
